ab6d0c060dde1f1f34ceb49124ee27c99a460de3
Alan Jordan <alanpjordan@gmail.com>
2017-06-07 19:48:12 +1200

    Test suite now set up.

--- tests/server/helpers/database-config.js
+++ tests/server/helpers/database-config.js
@@ -2,10 +2,9 @@ var knex = require('knex')
 var config = require('../../../knexfile').test
 
 module.exports = (test, createServer) => {
-  console.log(createServer);
   test.beforeEach(function (t) {
     t.context.connection = knex(config)
-    if (createServer) t.context.server = createServer(t.context.connection)
+    if (createServer) t.context.app = createServer(t.context.connection)
     return t.context.connection.migrate.latest()
       .then(function () {
         return t.context.connection.seed.run()
--- tests/server/routes.test.js
+++ tests/server/routes.test.js
@@ -6,13 +6,14 @@ var createServer = require('../../server/server')
 var configureDatabase = require('./helpers/database-config')
 configureDatabase(test, createServer)
 
-test('GET /users/', (t) => {
+
+test('GET /users/', t => {
   return request(t.context.app)
-    .get('/api/v1/user')
+    .get('/api/v1/users')
     .expect(200)
     .then((result) => {
       return new Promise((resolve, reject) => {
-        t.is(result.length, 2)
+        t.is(result.body.length, 2)
         resolve()
       })
     })
