da410ec52cbc07ee5d62f8eb0ef412b8ce36f19e
Alan Jordan <alanpjordan@gmail.com>
2017-06-13 15:57:24 +1200

    Still fixing up testing suite

--- tests/client/Home.test.js
+++ tests/client/Home.test.js
@@ -1,12 +1,27 @@
 import test from 'ava'
 import React from 'react'
+import {Provider} from 'react-redux'
 import { shallow, mount, render } from 'enzyme'
 
 import './helpers/setup-dom'
 import store from '../../client/store'
 import Home from '../../client/components/Home'
 
-test.skip('front page class exists', t => {
-  const wrapper = mount(<Home store={store} />)
+test('front page class exists', t => {
+  const wrapper = mount(
+    <Provider store={store}>
+      <Home />
+    </Provider>
+  )
   t.is(wrapper.find('.frontPage').exists(), true)
+})
+
+test('Four h2 tags on front page', t => {
+  const wrapper = mount(
+    <Provider store={store}>
+      <Home />
+    </Provider>
+  )
+  t.is(wrapper.find('.introText').children().length, 4)
+  t.is(wrapper.find('.introText').children().first().text(), 'Discover new games.')
 })
--- tests/client/actions.test.js
+++ tests/client/actions.test.js
@@ -1,10 +1,18 @@
 import test from 'ava'
+import nock from 'nock'
 
-import {fetchLatestUsers} from '../../client/actions'
-import users from '../../client/reducers/users'
+import * as action from '../../client/actions'
 
-test.skip('fetchLatestUsers gets 2 users', t => {
-  const nextState = users([], fetchLatestUsers())
-  t.is(nextState.length, 2)
-  t.is(nextState[1].user_username, 'itsame')
+test.cb('fetchLatestUsers', t => {
+  nock('http://localhost:80')
+    .get('/api/v1/latestusers')
+    .reply(200, [{user_username: 'eljordy'}, {user_username: 'itsame'}])
+
+  action.fetchLatestUsers()((actual) => {
+    t.is(actual.users.length, 2)
+    t.is(actual.users[1].user_username, 'itsame')
+    t.is(actual.type, 'RECEIVE_NEW_USERS')
+    nock.cleanAll()
+    t.end()
+  })
 })
