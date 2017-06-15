6f4b332d44bc9c1b99c3371031db82ebeefedf56
Alan Jordan <alanpjordan@gmail.com>
2017-06-15 15:01:24 +1200

    Pushing

--- client/components/NewUsers.jsx
+++ client/components/NewUsers.jsx
@@ -3,15 +3,15 @@ import React from 'react'
 const NewUsers = (props) => {
   function renderUsers () {
     return props.users.map((user, i) => {
-      return <li key={i}><a href='#'>{`${user.user_username}`}</a></li>
+      return (
+        <li key={i}><a href='#'>{`${user.user_username}`}</a></li>
+      )
     })
   }
 
   return (
-    <div>
-      <ul className='newUsers'>
-        {renderUsers()}
-      </ul>
+    <div className='newUsers'>
+      {renderUsers()}
     </div>
   )
 }
