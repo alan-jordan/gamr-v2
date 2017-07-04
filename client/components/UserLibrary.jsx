848faa133663a6e0b38679f4f94bf80cc2acaa6a
Alan Jordan <alanpjordan@gmail.com>
2017-07-04 16:15:01 +1200

    Working on user library

--- client/components/UserLibrary.jsx
+++ client/components/UserLibrary.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 
 const UserLibrary = (props) => {
+  function renderGames() {
+      //Needs to be an action to set games on the state.
+  }
 
   return (
     <div></div>
