b619a0ca74505e0ac7cc39b4db26db8a0ea35d34
Alan Jordan <alanpjordan@gmail.com>
2017-07-08 12:26:56 +1200

    Fixed broken test

--- client/components/UserLibrary.jsx
+++ client/components/UserLibrary.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 
 const UserLibrary = (props) => {
-  getUserGames(props.id) {
-  }
+  // getUserGames(props.id) {
+  // }
 
-  function renderGames() {
-      //Needs to be an action to set games on the state.
-  }
+  // function renderGames() {
+  //     //Needs to be an action to set games on the state.
+  // }
 
   return (
     <div></div>
