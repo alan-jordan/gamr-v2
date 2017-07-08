2c2e4659b78850d53f0d0aca32ea407002299b0e
Alan Jordan <alanpjordan@gmail.com>
2017-07-09 11:47:53 +1200

    fixed small issue on userinfolibrary where username not being rendered

--- client/components/UserInfoLibrary.jsx
+++ client/components/UserInfoLibrary.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 
 const UserInfoLibrary = (props) => {
-
   return (
     <div className = "row" > <div className="col-lg-5">
       <div className="media">
@@ -10,7 +9,7 @@ const UserInfoLibrary = (props) => {
           <img className="media-object dp img-circle" src="https://pbs.twimg.com/profile_images/3100419678/7a778ab4ba1872fa320d62d9fe7b0398_400x400.jpeg" />
         </a>
         <div className="media-body">
-          <h4 className="media-heading">{props.user.user_username}
+          <h4 className="media-heading">{props.user.user.user_username}
             <small></small>
           </h4>
           {/* <h5>Software Developer at
