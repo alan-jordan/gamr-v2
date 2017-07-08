c4907a1f383303a9407d2279ba664d79822cf041
Alan Jordan <alanpjordan@gmail.com>
2017-07-08 12:24:29 +1200

    Workign on glueing together back and front end to get users games

--- client/components/UserLibrary.jsx
+++ client/components/UserLibrary.jsx
@@ -2,11 +2,9 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 
 const UserLibrary = (props) => {
-  console.log(props);
-  function getUserGames() {
-
+  getUserGames(props.id) {
   }
-  
+
   function renderGames() {
       //Needs to be an action to set games on the state.
   }
