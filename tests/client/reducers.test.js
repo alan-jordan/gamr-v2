498dbcf9412fdbbca8abbfc16e43525c62ac81f7
Alan Jordan <alanpjordan@gmail.com>
2017-07-09 10:18:10 +1200

    setUserGames now working. Tests also passing

--- tests/client/reducers.test.js
+++ tests/client/reducers.test.js
@@ -1,19 +1,20 @@
 import test from 'ava'
 
-import userReducer from '../../client/reducers/users'
+import usersReducer from '../../client/reducers/users'
+import userReducer from '../../client/reducers/user'
 import {receiveNewUsers, setUserGames} from '../../client/actions'
 import gameReducer from '../../client/reducers/game'
 import * as userGames from './helpers/userGamesExample'
 
 test('default users is empty array', t => {
-  const initialState = userReducer()
+  const initialState = usersReducer()
   t.is(initialState.length, 0)
 })
 
 test('receiveNewUsers works', t => {
-  const initialState = userReducer()
+  const initialState = usersReducer()
   const newUsers = [{user_username: 'eljordy'}, {user_username: 'itsame'}]
-  const nextState = userReducer(initialState, receiveNewUsers(newUsers))
+  const nextState = usersReducer(initialState, receiveNewUsers(newUsers))
   t.is(nextState.length, 2)
   t.is(nextState[1].user_username, 'itsame')
 })
@@ -26,5 +27,6 @@ test('default game is an empty object', t => {
 test('user games are set', t => {
   const initialState = userReducer()
   const nextState = userReducer(initialState, setUserGames(userGames.userGames))
-  console.log(nextState);
+  t.is(nextState.length, 5)
+  t.is(nextState[2].igdb_game_id, 18)
 })
