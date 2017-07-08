ac04a95e66de6f9a1686e2c893be4a0bac901a26
Alan Jordan <alanpjordan@gmail.com>
2017-07-08 12:20:30 +1200

    Updated webpack config to resolve issue with dotenv

--- client/components/UserLibrary.jsx
+++ client/components/UserLibrary.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 
 const UserLibrary = (props) => {
+  console.log(props);
+  function getUserGames() {
+
+  }
+  
   function renderGames() {
       //Needs to be an action to set games on the state.
   }
--- webpack.config.js
+++ webpack.config.js
@@ -14,5 +14,8 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx']
   },
-  devtool: 'source-map'
+  devtool: 'source-map',
+  node: {
+    fs: 'empty'
+  }
 }
