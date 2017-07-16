e768254b017d1099224dba6ded6d4bfd4f1d60f8
Alan Jordan <alanpjordan@gmail.com>
2017-07-16 15:04:18 +1200

    Add game toggle working on add game page

--- client/components/AddGame.jsx
+++ client/components/AddGame.jsx
@@ -4,7 +4,6 @@ import {addGameVisibleToggle} from '../actions/'
 
 class AddGame extends React.Component {
 
-  // Need to create an add visible toggle off and on
   // Then some checking on whether a user has a game or not already
   // Then some glue back to the API to post some datas
 
@@ -15,14 +14,19 @@ class AddGame extends React.Component {
   render() {
     return (
       <div className='addGame'>
-        <h2 onClick={(e) => this.addGameToggle(e)}>Add Game</h2>
+        {this.props.addGame ?
+          <div>
+            <p>Blahs</p>
+            <h2 onClick={(e) => this.addGameToggle(e)}>cancel</h2>
+          </div>
+          : <h2 onClick={(e) => this.addGameToggle(e)}>Add Game</h2>
+        }
       </div>
     )
   }
 }
 
 const mapStateToProps = (state) => {
-  console.log(state);
   return {addGame: state.addGame}
 }
 
