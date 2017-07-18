a2d98623310492084ca0deb9313b7856cab78409
Alan Jordan <alanpjordan@gmail.com>
2017-07-19 11:19:25 +1200

    Started adding stuff for passport. Basing this on Gurgl auth initially

--- server/server.js
+++ server/server.js
@@ -3,6 +3,10 @@ var express = require('express')
 var bodyParser = require('body-parser')
 const cors = require('cors')
 
+const passport = require('passport')
+const LocalStrategy = require('passport-local')
+const auth = require('./lib/auth')
+
 var users = require('./routes/users')
 var games = require('./routes/games')
 var search = require('./routes/search')
@@ -23,6 +27,9 @@ app.use(express.static(path.join(__dirname, '../public')))
 app.use('/api/v1', users)
 app.use('/api/v1', games)
 app.use('/api/v1', search)
+
+app.use(passport.initialize())
+passport.use(new LocalStrategy(auth.verify))
 
 module.exports = (connection) => {
   app.set('connection', connection)
