dce6f6061584611748770acaa9aa59d89db8e5b9
Alan Jordan <alanpjordan@gmail.com>
2017-07-23 16:14:23 +1200

    Changes migrations and seeds to remove usernames and passwords from being exposed via api

--- migrations/20170607160141_users.js
+++ migrations/20170607160141_users.js
@@ -2,13 +2,10 @@
 exports.up = function (knex, Promise) {
   return knex.schema.createTable('users', function (table) {
     table.increments('id').primary()
-    table.string('user_email')
-    table.unique('user_email')
     table.string('user_username')
+    table.unique('user_username')
     table.string('user_first_name')
     table.string('user_surname')
-    table.date('user_dob')
-    table.binary('user_password')
     table.date('user_date_registered').defaultTo(knex.fn.now())
   })
 }
