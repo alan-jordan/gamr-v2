1cd9101f77a791da2c8238d43d8b0d117a5cdfaf
Alan Jordan <alanpjordan@gmail.com>
2017-07-24 09:46:48 +1200

    Converting header to flexbox

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='row header'>
-      <div className='ten columns'>
-        <a  className='logo' href='/'>gamr</a>
+    <div className='header'>
+      <div className='logo'>
+        <a  className='headerItem' href='/'>gamr</a>
       </div>
-      <div className='two columns'>
-        <a className='loginHeader' href='/#/login'>login</a>
+      <div className='loginHeader'>
+        <a className='headerItem' href='/#/login'>login</a>
       </div>
     </div>
   )
